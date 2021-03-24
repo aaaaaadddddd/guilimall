7645ca12e8cc0b5bba97de283ccd2219000e3ace
luojie_global <1171156631@qq.com>
2021-03-24 20:57:58 +0800

    初步搭建五个微服务

---
+++ guilimall-product/src/test/java/com/ecology/guilimall/guilimallproduct/GuilimallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecology.guilimall.guilimallproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuilimallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
